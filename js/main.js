6d00fc2d161934e7380eaf27e0d29a2fe5633ba2
Silvana Barraza <barrazasilvana@gmail.com>
2023-02-24 11:12:13 -0300

    Se crearon las estructuras de base en html, las carpetas y archivos css y js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+console.log("Mi cv")
+
+document.getElementById("boton").onclick = function(){
+    document.getElementById("about_me").innerHTML="Soy diseñadora UX/UI, desarrolladora web frontend y también licenciada en nutrición. Durante la pandemia, conoci estas nuevas herramientas, uno de mis objetivos es poder seguir este camino laboral y profesionalmente, capacitandome, adquiriendo nuevas habilidades y llevandolas a la práctica. Y por qué no, cuando sea necesario, darle mi persepectiva en relación a la salud y la nutricón cuando sea necesario."
+}
